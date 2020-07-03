6f910e08e870f8b4ddc097bfa407c2f19be665cc
vishnu prasath <30981535+teamdraftbox@users.noreply.github.com>
2020-07-03 18:19:00 +0530

    Join userGroup Button for group header (#10)
    * join userGroup Button
    
    usergroup join format
    
    * join userGroup Button
    
    usergroup join format
    
    code optimization group join btn
    
    * join user group responsive fix
    
    * join group responsive fix
    
    * :recycle: style cleanup + tablet view fixes
    
    Co-authored-by: Vishnu Prasath <vishnu.prasath@sneed.in>
    Co-authored-by: Harsh Zalavadiya <harsh@foobars.in>

--- src/components/@core/navigation-menu/light/left-menu/logo.tsx
+++ src/components/@core/navigation-menu/light/left-menu/logo.tsx
@@ -5,12 +5,20 @@ import { useStoreState } from "easy-peasy";
 import { Mq } from "mq-styled-components";
 import React from "react";
 
+import JoinUserGroup from "../join-group";
+
 const Logo = styled.div`
+  width: fit-content;
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 0.25rem 0;
   min-height: 3.4rem;
+  flex-wrap: wrap;
+
+  .right-logo {
+    display: contents;
+  }
 
   a {
     display: flex;
@@ -18,21 +26,36 @@ const Logo = styled.div`
     line-height: 1.2rem;
     img {
       height: 3.75rem;
-      width: 8rem;
+      max-width: 8rem;
       object-fit: contain;
     }
   }
 
-  button {
+  .menu-toggle {
     display: none;
     padding: 0.5rem;
     font-size: 1.5rem;
   }
 
+  .join-usergroup {
+    margin-left: 0.75rem;
+  }
+
   ${Mq.max.sm} {
     width: 100%;
-    button {
+
+    p {
+      max-width: 6rem;
+    }
+
+    .menu-toggle {
       display: initial;
+    }
+
+    .join-usergroup {
+      flex-basis: 100%;
+      margin: 0.75rem 0;
+      width: 100%;
     }
   }
 
@@ -42,7 +65,10 @@ const Logo = styled.div`
 `;
 
 export default function PrimaryLogo({ isOpen, onToggle }) {
-  const { name, icon } = useStoreState((s) => s.currentGroup);
+  const {
+    currentGroup: { name, icon, id },
+    isLoggedIn
+  } = useStoreState((s) => s);
 
   return (
     <Logo>
@@ -52,9 +78,10 @@ export default function PrimaryLogo({ isOpen, onToggle }) {
           <Text ml={2}>{name}</Text>
         </Link>
       </LocalLink>
-      <button onClick={onToggle} aria-label="toggle primary menu">
+      <button className="menu-toggle" onClick={onToggle} aria-label="toggle primary menu">
         <Icon name={isOpen ? "ibpcross" : "ibpmenu"} />
       </button>
+      {isLoggedIn && id && <JoinUserGroup />}
     </Logo>
   );
 }
