2ecfe0a4de4363a93b2810cc52bba7262fdb49dc
Sethuraman Srinivasan <sethu10121994@gmail.com>
2020-07-14 16:24:42 +0530

    :recycle: open linked page everytime

--- worker/index.js
+++ worker/index.js
@@ -27,20 +27,8 @@ self.addEventListener("notificationclick", function (event) {
       type: "window",
       includeUncontrolled: true
     })
-    .then((windowClients) => {
-      let matchingClient = null;
-      for (let i = 0; i < windowClients.length; i++) {
-        const windowClient = windowClients[i];
-        if (windowClient.url === feClickAction) {
-          matchingClient = windowClient;
-          break;
-        }
-      }
-      if (matchingClient) {
-        return matchingClient.focus();
-      } else {
-        return clients.openWindow(feClickAction);
-      }
+    .then(() => {
+      return clients.openWindow(feClickAction);
     });
   event.waitUntil(promiseChain);
 });
