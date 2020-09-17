8947e925268a9509137562679c42f47faf455587
harshzalavadiya <harsh@strandls.com>
2020-09-17 20:20:47 +0530

    :recycle: elipsis + auto select

--- src/components/pages/group/edit/group-rules/group-rules-table/group-rules-row.tsx
+++ src/components/pages/group/edit/group-rules/group-rules-table/group-rules-row.tsx
@@ -1,8 +1,6 @@
-import { Button } from "@chakra-ui/core";
+import { Box, Button } from "@chakra-ui/core";
 import useTranslation from "@configs/i18n/useTranslation";
 import React from "react";
-
-import ShowLess from "./show-less";
 
 const GroupRulesRow = ({ itemDetails, onDelete }) => {
   const { t } = useTranslation();
@@ -11,7 +9,11 @@ const GroupRulesRow = ({ itemDetails, onDelete }) => {
   return (
     <tr>
       <td>{name}</td>
-      <td>{name === "spatialRule" ? <ShowLess value={value} /> : value}</td>
+      <td>
+        <Box userSelect="all" className="elipsis">
+          {value}
+        </Box>
+      </td>
       <td>
         <Button onClick={onDelete} variant="link" variantColor="red" leftIcon="delete" ml={2}>
           {t("DELETE")}
