c545a0e9aa53033309e0b24557cab2992efa6462
Vishnu Prasath <vishnu.prasath@sneed.in>
2020-10-30 18:41:13 +0530

    info tab re-align ment

--- src/components/pages/document/list/view/list/tabs/infotab.tsx
+++ src/components/pages/document/list/view/list/tabs/infotab.tsx
@@ -1,15 +1,4 @@
-import {
-  Avatar,
-  Badge,
-  Box,
-  Flex,
-  Heading,
-  Image,
-  Link,
-  SimpleGrid,
-  Stack,
-  Text
-} from "@chakra-ui/core";
+import { Avatar, Badge, Box, Flex, Heading, Image, Link, Stack, Text } from "@chakra-ui/core";
 import FlagActionButton from "@components/@core/action-buttons/flag";
 import LocalLink from "@components/@core/local-link";
 import FilterIconsList from "@components/pages/document/common/filter-list-icon";
@@ -22,7 +11,6 @@ import LocationIcon from "@icons/location";
 import LockIcon from "@icons/lock";
 import PeopleIcon from "@icons/people";
 import { axFlagDocument, axUnFlagDocument } from "@services/document.service";
-import { formatDateReadable } from "@utils/date";
 import { getUserImage } from "@utils/media";
 import React from "react";
 
@@ -32,40 +20,40 @@ export default function InfoTab({ document, habitatIds, specieIds, flags }) {
   const { user, currentGroup } = useGlobalState();
 
   return (
-    <Box
+    <Stack
       color="gray.600"
       boxSize="full"
       display="flex"
       flexDir="column"
-      justifyContent="space-between"
+      justifyContent="space-evenly"
     >
-      <Stack ml={4}>
-        <LocalLink href={`/document/show/${document.id}`}>
-          <a>
-            <Heading size="sm" title={document?.title}>
-              <Flex justifyContent="flex-start" alignItems="center">
-                <Image boxSize="3rem" src="/next-assets/document.svg" />
-                <Text m={2}>
-                  {document?.title || t("OBSERVATION.UNKNOWN")}
-                  <Badge ml={2} colorScheme="red">
-                    {document.item_type}
-                  </Badge>
-                  <FlagActionButton
-                    resourceId={document.id}
-                    resourceType="document"
-                    initialFlags={flags}
-                    userId={user.id}
-                    flagFunc={axFlagDocument}
-                    unFlagFunc={axUnFlagDocument}
-                  />
-                </Text>
-              </Flex>
-            </Heading>
-          </a>
-        </LocalLink>
-        <Flex justifyContent="flex-start">
+      <LocalLink href={`/document/show/${document.id}`}>
+        <a>
+          <Heading size="sm" title={document?.title}>
+            <Flex justifyContent="flex-start" alignItems="center">
+              <Image boxSize="3rem" src="/next-assets/document.svg" />
+              <Text isTruncated maxWidth="700px" m={2}>
+                {document?.title || t("OBSERVATION.UNKNOWN")}
+              </Text>
+              <Badge ml={2} colorScheme="red">
+                {document.item_type}
+              </Badge>
+              <FlagActionButton
+                resourceId={document.id}
+                resourceType="document"
+                initialFlags={flags}
+                userId={user.id}
+                flagFunc={axFlagDocument}
+                unFlagFunc={axUnFlagDocument}
+              />
+            </Flex>
+          </Heading>
+        </a>
+      </LocalLink>
+      <Stack>
+        <Stack ml={2} justifyContent="center">
           {document?.author && (
-            <Text mr={2} title="Author">
+            <Text isTruncated maxWidth="300px" mr={2} title="Author">
               <PeopleIcon mb={1} mr={2} /> {document.author}
             </Text>
           )}
@@ -73,61 +61,62 @@ export default function InfoTab({ document, habitatIds, specieIds, flags }) {
           {document.created_on && (
             <Text title="Date">
               <CalendarIcon mb={1} mr={2} />
-              {formatDateReadable(document.created_on)}
+              {document.year}
             </Text>
           )}
-        </Flex>
-        {document?.notes && (
-          <Box>
-            <Text title="Abstract">
-              {document?.notes?.replace(/<[^>]*>?/gm, "").substring(0, 200)}
-            </Text>
-          </Box>
-        )}
-      </Stack>
-      <Flex justifyContent="space-between" m={4} alignItems="center">
-        <SimpleGrid columns={[1, 2, 2]}>
-          <Link href={`${currentGroup?.webAddress}/user/show/${user?.id}`}>
-            <Flex alignItems="flex-end">
-              <Avatar
-                mr={2}
-                flexShrink={0}
-                size="sm"
-                name={user?.name}
-                src={getUserImage(user?.profilePic)}
-              />
-              <Text>{user?.name}</Text>
-            </Flex>
-          </Link>
-          {document.publisher && (
-            <Text title="Publisher">
-              <LockIcon mb={1} m={3} />
-              {document.publisher}
-            </Text>
-          )}
-        </SimpleGrid>
 
-        <Flex>
-          {habitatIds[0] !== null && habitats && (
-            <FilterIconsList
-              title={t("GROUP.HABITATS_COVERED")}
-              type="habitat"
-              icon={<Grid />}
-              filterIds={habitatIds}
-              filterList={habitats}
-            />
+          {document?.notes && (
+            <Box>
+              <Text isTruncated maxWidth="300px" title="Abstract">
+                {document?.notes?.replace(/<[^>]*>?/gm, "")}
+              </Text>
+            </Box>
           )}
-          {habitatIds[0] !== null && species && (
-            <FilterIconsList
-              title={t("GROUP.SPECIES_COVERAGE")}
-              type="species"
-              icon={<LocationIcon />}
-              filterIds={specieIds}
-              filterList={species}
-            />
-          )}
+        </Stack>
+
+        <Flex justifyContent="space-between" alignItems="center">
+          <Stack isInline spacing={5} align="center">
+            <Link href={`${currentGroup?.webAddress}/user/show/${user?.id}`}>
+              <Flex alignItems="center">
+                <Avatar
+                  m={1}
+                  flexShrink={0}
+                  size="sm"
+                  name={user?.name}
+                  src={getUserImage(user?.profilePic)}
+                />
+                <Text>{user?.name}</Text>
+              </Flex>
+            </Link>
+            {document.publisher && (
+              <Text title="Publisher">
+                <LockIcon m={2} />
+                {document.publisher}
+              </Text>
+            )}
+          </Stack>
+          <Stack isInline spacing={5} align="center">
+            {habitatIds[0] !== null && habitats && (
+              <FilterIconsList
+                title={t("GROUP.HABITATS_COVERED")}
+                type="habitat"
+                icon={<Grid />}
+                filterIds={habitatIds}
+                filterList={habitats}
+              />
+            )}
+            {habitatIds[0] !== null && species && (
+              <FilterIconsList
+                title={t("GROUP.SPECIES_COVERAGE")}
+                type="species"
+                icon={<LocationIcon />}
+                filterIds={specieIds}
+                filterList={species}
+              />
+            )}
+          </Stack>
         </Flex>
-      </Flex>
-    </Box>
+      </Stack>
+    </Stack>
   );
 }
