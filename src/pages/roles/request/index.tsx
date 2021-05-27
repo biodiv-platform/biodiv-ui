f2f1e9ed68a1afdabdb628e686662b5bad496959
harshzalavadiya <harsh@strandls.com>
2021-05-27 17:45:33 +0530

    refactor: :recycle: admin can request permission for other users

--- src/pages/roles/request/index.tsx
+++ src/pages/roles/request/index.tsx
@@ -1,6 +1,21 @@
-import SpeciesTaxonBrowserComponent from "@components/pages/species/taxonBrowser";
+import { authorizedPageSSR } from "@components/auth/auth-redirect";
+import RolesRequestComponent from "@components/pages/roles/request";
+import { Role } from "@interfaces/custom";
+import { hasAccess } from "@utils/auth";
 import React from "react";
 
-export default function SpeciesTaxonBrowserPage() {
-  return <SpeciesTaxonBrowserComponent />;
+export default function RolesRequestPage({ isAdmin }) {
+  return <RolesRequestComponent isAdmin={isAdmin} />;
 }
+
+export async function getServerSideProps(ctx) {
+  authorizedPageSSR([Role.Any], ctx, true);
+
+  return {
+    props: {
+      isAdmin: hasAccess([Role.Admin], ctx)
+    }
+  };
+}
+
+hasAccess([Role.Admin]);
