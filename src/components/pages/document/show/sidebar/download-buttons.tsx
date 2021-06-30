3fb398830eae320e2698252f5c122b72db9253bf
harshzalavadiya <harsh@strandls.com>
2021-06-30 22:35:50 +0530

    refactor: :recycle: added zbib on cite button

--- src/components/pages/document/show/sidebar/download-buttons.tsx
+++ src/components/pages/document/show/sidebar/download-buttons.tsx
@@ -2,6 +2,7 @@ import { Button, SimpleGrid } from "@chakra-ui/react";
 import DownloadIcon from "@icons/download";
 import PeopleIcon from "@icons/people";
 import { axDownloadDocument } from "@services/document.service";
+import { isBrowser } from "@static/constants";
 import { waitForAuth } from "@utils/auth";
 import { sendFileFromResponse } from "@utils/download";
 import { getDocumentFilePath } from "@utils/media";
@@ -21,6 +22,12 @@ export default function DownloadButtons({ documentPath, documentId, title }) {
     }
   };
 
+  const openZbib = () => {
+    if (isBrowser) {
+      window.open(`https://zbib.org/import?q=${window.location.href}`, "_blank")?.focus();
+    }
+  };
+
   return (
     <SimpleGrid columns={{ base: 1, md: 2 }} mb={4} spacing={4}>
       <Button
@@ -32,7 +39,7 @@ export default function DownloadButtons({ documentPath, documentId, title }) {
       >
         {t("document:download.pdf")}
       </Button>
-      <Button variant="outline" leftIcon={<PeopleIcon />} isDisabled={true} colorScheme="teal">
+      <Button variant="outline" leftIcon={<PeopleIcon />} onClick={openZbib} colorScheme="teal">
         {t("document:download.citation")}
       </Button>
     </SimpleGrid>
