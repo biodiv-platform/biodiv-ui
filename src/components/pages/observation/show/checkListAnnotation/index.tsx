be1172e700bbdd55980218bb533e5b72a8e5a406
harshzalavadiya <harsh@strandls.com>
2021-09-07 20:56:01 +0530

    refactor: :recycle key rename

--- src/components/pages/observation/show/checkListAnnotation/index.tsx
+++ src/components/pages/observation/show/checkListAnnotation/index.tsx
@@ -10,7 +10,7 @@ export default function CheckListAnnotation({ customData }) {
 
   return (
     <Box mb={4} className="white-box">
-      <BoxHeading>📚 {t("observation:checklistAnnotation")}</BoxHeading>
+      <BoxHeading>📚 {t("observation:checklist_annotation")}</BoxHeading>
       <SimpleGrid columns={[1, 1, 5, 5]} spacing={2}>
         {Object.keys(customData).map(
           (key, index) =>
