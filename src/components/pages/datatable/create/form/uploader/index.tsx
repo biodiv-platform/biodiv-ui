93d50748f24f2c11587266f604f8c635c77767f6
vishnu prasath <teamdraftbox@gmail.com>
2021-11-03 18:07:05 +0530

    Update index.tsx

--- src/components/pages/datatable/create/form/uploader/index.tsx
+++ src/components/pages/datatable/create/form/uploader/index.tsx
@@ -7,7 +7,6 @@ import IndexedDBProvider from "use-indexeddb";
 import DropzoneField, { IDropzoneProps } from "./field";
 
 const DropzoneFieldContainer = (props: IDropzoneProps) => {
-
   const form = useFormContext();
 
   return (
