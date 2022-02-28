eb10a1fdcb80257f5fc39fc94a3f92447dcf99e4
Harsh Zalavadiya <harshzalavadiya@users.noreply.github.com>
2022-02-28 14:39:57 +0530

    feat: :sparkles: mail unsubscribe (#195) (#197)
    * refactor: :recycle: json sort
    
    * chore: :robot: added translations + missing
    
    * feat: :sparkles: unsubscribe page
    
    * chore: :robot: dependency updates
    
    * refactor: :reccyle: removed sendDigest option

---
+++ src/pages/user/unsubscribe/[url].tsx
@@ -0,0 +1,11 @@
+import UserUnsubscribePageComponent from "@components/pages/user/unsubscribe";
+import React from "react";
+
+export default function UserUnsubscribePage() {
+  return <UserUnsubscribePageComponent />;
+}
+
+UserUnsubscribePage.config = {
+  header: false,
+  footer: false
+};
