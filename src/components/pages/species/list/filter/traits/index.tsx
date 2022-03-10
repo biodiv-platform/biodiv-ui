4085495ca6a6a4764b0e4d02717ddae2d6f0a665
Harsh Zalavadiya <harshzalavadiya@users.noreply.github.com>
2022-03-10 16:25:46 +0530

    Species list color (#202) (#205)
    * feat: :sparkles: mail unsubscribe (#195)
    
    * refactor: :recycle: json sort
    
    * chore: :robot: added translations + missing
    
    * feat: :sparkles: unsubscribe page
    
    * chore: :robot: dependency updates
    
    * refactor: :reccyle: removed sendDigest option
    
    * 🎨 added color filter species list page
    
    * ♻️ code refactoring and datatable mapping dropdown ui fix
    
    Co-authored-by: Harsh Zalavadiya <harshzalavadiya@users.noreply.github.com>
    
    Co-authored-by: vishnu prasath <teamdraftbox@gmail.com>

--- src/components/pages/species/list/filter/traits/index.tsx
+++ src/components/pages/species/list/filter/traits/index.tsx
@@ -9,19 +9,21 @@ import React, { useMemo } from "react";
 
 import useSpeciesList from "../../use-species-list";
 import CheckboxFilterPanel from "../shared/checkbox";
+import HsvColorFilter from "../shared/hsv-color-picker";
 import SubAccordion from "../shared/sub-accordion";
 
+const includedDataType = ["COLOR", "STRING"]
 const filterTraitsfromAggregation = (traits, ag) => {
   if (Object.keys(ag?.groupTraitsName)?.length <= 0) return [];
-
   return traits
     .map(({ categoryName, traitsValuePairList }) => ({
       categoryName,
-      traitsValuePairList: traitsValuePairList.filter((f) =>
-        Object.keys(ag.groupTraitsName).includes(`${f.traits.id}`)
-      )
+      traitsValuePairList: traitsValuePairList
+      .filter(({ traits: trait }) => includedDataType.includes(trait.dataType))
+      .filter((f) =>
+          !Object.keys(ag.groupTraitsName).includes(f.traits.id)
+        )
     }))
-    .filter((i) => i?.traitsValuePairList?.some((item) => item.values.length > 0));
 };
 export default function TraitsFilter() {
   const {
@@ -46,20 +48,33 @@ export default function TraitsFilter() {
               <SubAccordion>
                 <AccordionPanel>
                   {isExpanded &&
-                    traitsValuePairList?.map(({ traits: trait, values }) => (
-                      <CheckboxFilterPanel
-                        key={trait.id}
-                        label={trait.name}
-                        filterKey={"traits"}
-                        options={values.map(({ value, icon, id }) => ({
-                          label: value,
-                          valueIcon: icon,
-                          value: id.toString()
-                        }))}
-                        statKey={`groupTraits`}
-                        skipOptionsTranslation={true}
-                      />
-                    ))}
+                    traitsValuePairList?.map(({ traits: trait, values }) => {
+                      switch (trait.dataType) {
+                        case "COLOR":
+                          return (
+                            <HsvColorFilter
+                              filterKey={`trait_${trait.id}.color_hsl`}
+                              label={trait.name}
+                            />
+                          );
+
+                        default:
+                          return (
+                            <CheckboxFilterPanel
+                              key={trait.id}
+                              label={trait.name}
+                              filterKey={`trait_${trait.id}.string`}
+                              options={values.map(({ value, icon, id }) => ({
+                                label: value,
+                                valueIcon: icon,
+                                value: id.toString()
+                              }))}
+                              statKey={`groupTraits`}
+                              skipOptionsTranslation={true}
+                            />
+                          );
+                      }
+                    })}
                 </AccordionPanel>
               </SubAccordion>
             </>
