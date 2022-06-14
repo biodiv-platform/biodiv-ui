af298b399a1aab1dbe57bd6dd47078d016d202ce
Harsh Zalavadiya <harsh@strandls.com>
2022-06-14 18:09:10 +0530

    refactor: :recycle: cleanup

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -2,7 +2,7 @@ import ErrorPage from "@components/pages/_error";
 import React from "react";
 
 function ErrorInt({ statusCode }) {
-  return <ErrorPage statusCode={statusCode || 404} />;
+  return <ErrorPage statusCode={statusCode} />;
 }
 
 export default ErrorInt;
