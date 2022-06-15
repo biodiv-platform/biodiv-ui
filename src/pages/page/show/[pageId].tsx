fec2a5cf3af7faedefcc245dd6e5648b1e84c55a
Harsh Zalavadiya <harsh@strandls.com>
2022-06-15 19:50:52 +0530

    refactor: :recycle: lh improvements

--- src/pages/page/show/[pageId].tsx
+++ src/pages/page/show/[pageId].tsx
@@ -2,10 +2,8 @@ import PageShowPageComponent from "@components/pages/page/show";
 import { axGetPageByID } from "@services/pages.service";
 import React from "react";
 
-import Error from "../../_error";
-
-export default function PageShowPage({ success, data }) {
-  return success ? <PageShowPageComponent page={data} /> : <Error statusCode={404} />;
+export default function PageShowPage({ data }) {
+  return <PageShowPageComponent page={data} />;
 }
 
 export const getServerSideProps = async (ctx) => {
