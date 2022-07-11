b463359df440dcca280924f3003bd73aa99504b6
arun <arunasd463@outlook.com>
2022-07-11 10:19:59 +0530

    gallery-slider edit feature

---
+++ src/components/pages/group/edit/homepage-customization/gallery-setup/gallery-setup-form/editfrom.tsx
@@ -0,0 +1,74 @@
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { Box, Button } from "@chakra-ui/react";
+import { SubmitButton } from "@components/form/submit-button";
+import { TextAreaField } from "@components/form/textarea";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { axEditHomePageGallery } from "@services/usergroup.service";
+import { NotificationType } from "@utils/notification";
+import notification from "@utils/notification";
+import useTranslation from "next-translate/useTranslation";
+import React, { useEffect, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+
+import { galleryFieldValidationSchema } from "./common";
+import NewResourceForm from "./new-resource-form";
+
+export default function GalleryEditFrom({ isEdit, setGalleryList, editGalleryData }) {
+  const { t } = useTranslation();
+  const [defaultValues] = useState<any>();
+
+  const { id, ugId, title, fileName, customDescripition, moreLinks, displayOrder } =
+    editGalleryData;
+
+  const hForm = useForm<any>({
+    mode: "onChange",
+    resolver: yupResolver(galleryFieldValidationSchema),
+    defaultValues: {
+      id,
+      ugId,
+      title,
+      fileName,
+      customDescripition,
+      moreLinks,
+      displayOrder
+    }
+  });
+
+  const handleFormSubmit = async (value) => {
+    const payload = {
+      ...value
+    };
+
+    const { success, data } = await axEditHomePageGallery(ugId, id, payload);
+    if (!success) {
+      notification(t("group:homepage_customization.update.failure"), NotificationType.Success);
+    }
+    notification(t("group:homepage_customization.update.success"), NotificationType.Success);
+    // data.gallerSlider returns a list of all galleryslider
+    setGalleryList(data.gallerySlider);
+    isEdit(false);
+  };
+
+  useEffect(() => {
+    hForm.reset(defaultValues);
+  }, [defaultValues]);
+
+  return (
+    <FormProvider {...hForm}>
+      <form onSubmit={hForm.handleSubmit(handleFormSubmit)}>
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Button m={3} type="button" onClick={() => isEdit(false)} leftIcon={<ArrowBackIcon />}>
+            {t("group:homepage_customization.back")}
+          </Button>
+        </Box>
+        <NewResourceForm />
+        <TextAreaField
+          name="customDescripition"
+          label={t("group:homepage_customization.table.description")}
+        />
+
+        <SubmitButton>{t("common:update")}</SubmitButton>
+      </form>
+    </FormProvider>
+  );
+}
--- src/services/usergroup.service.ts
+++ src/services/usergroup.service.ts
@@ -425,3 +425,16 @@ export const getAuthorizedUserGroupById = async () => {
     return { success: false };
   }
 };
+
+export const axEditHomePageGallery = async (userGroupId, galleryId, payload) => {
+  try {
+    const { data } = await http.put(
+      `${ENDPOINT.USERGROUP}/v1/group/homePage/edit/${userGroupId}/${galleryId}`,
+      payload
+    );
+    return { success: true, data };
+  } catch (e) {
+    console.error(e);
+    return { success: false, data: [] };
+  }
+};
