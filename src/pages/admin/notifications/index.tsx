87f2e67b0a439877b2c80e9cd3008ef3511255a7
Harsh Zalavadiya <harsh@strandls.com>
2022-07-14 15:46:44 +0530

    refactor: :recycle: admin auth check fix

--- src/pages/admin/notifications/index.tsx
+++ src/pages/admin/notifications/index.tsx
@@ -1,12 +1,13 @@
-import { authorizedPageSSR } from "@components/auth/auth-redirect";
+import { authorizedPageSSP } from "@components/auth/auth-redirect";
 import NotificationsComponent from "@components/pages/admin/notifications";
 import { Role } from "@interfaces/custom";
 import React from "react";
 
 const NotificationsPage = () => <NotificationsComponent />;
 
-NotificationsPage.getInitialProps = async (ctx) => {
-  authorizedPageSSR([Role.Admin], ctx, false);
+export const getServerSideProps = async (ctx) => {
+  const redirect = authorizedPageSSP([Role.Any], ctx);
+  return redirect || { props: {} };
 };
 
 export default NotificationsPage;
