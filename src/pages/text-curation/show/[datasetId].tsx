83fe0e2003b4dbbfc46093686b8dc603278ceefa
prakhar-s <prakhardevsingh4@gmail.com>
2022-08-26 15:27:12 +0530

    set ssr false

--- src/pages/text-curation/show/[datasetId].tsx
+++ src/pages/text-curation/show/[datasetId].tsx
@@ -1,9 +1,13 @@
 import { CurateEditProvider } from "@components/pages/text-curation/edit/use-curate-edit";
-import CurateShowPageComponent from "@components/pages/text-curation/show";
 import { Role } from "@interfaces/custom";
 import { axShowDataset } from "@services/curate.service";
 import { getParsedUser, hasAccess } from "@utils/auth";
+import dynamic from "next/dynamic";
 import React from "react";
+
+const CurateShowPageComponent = dynamic(() => import("@components/pages/text-curation/show"), {
+  ssr: false
+});
 
 export default function CurateEditPage({ data, datasetId, canEdit, userName, canValidate }) {
   return (
@@ -27,9 +31,9 @@ export const getServerSideProps = async (ctx) => {
 
   const userName = Object.keys(user).length === 0 ? "" : user.name;
   const canEdit =
-    data.contributors.includes(user?.id.toString()) ||
+    data.contributors.includes(user?.id?.toString()) ||
     hasAccess([Role.Admin], ctx) ||
-    data.validators.includes(user?.id.toString());
+    data.validators.includes(user?.id?.toString());
 
   const canValidate = data.validators.includes(user?.id?.toString());
 
