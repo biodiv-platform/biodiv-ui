75ce1f0621ad871b4e2da833401474a5db785c94
teamdraftbox <teamdraftbox@gmail.com>
2023-02-02 17:17:02 +0530

    🐛  custom field  options image fix

--- src/components/pages/observation/create/form/custom-field-form/custom-field-options.tsx
+++ src/components/pages/observation/create/form/custom-field-form/custom-field-options.tsx
@@ -1,10 +1,13 @@
 import { Image, Stack, Text } from "@chakra-ui/react";
-import { getGroupImageThumb } from "@utils/media";
+import { getResourceThumbnail, RESOURCE_CTX } from "@utils/media";
 import React, { useMemo } from "react";
 import { components } from "react-select";
 
 export default function CustomFieldOption(props: any) {
-  const imageURL = useMemo(() => getGroupImageThumb(props.data.iconURL), [props.data.iconURL]);
+  const imageURL = useMemo(
+    () => getResourceThumbnail(RESOURCE_CTX.USERGROUPS, props.data.iconURL, 32),
+    [props.data.iconURL]
+  );
   return (
     <components.Option {...props}>
       <Stack isInline={true} alignItems="center">
