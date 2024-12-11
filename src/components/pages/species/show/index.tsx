9b1b981895eb1b13afe5d073deb7f232ecde93e9
prakhar-s <prakhardevsingh4@gmail.com>
2024-12-11 14:16:42 +0000

    refactor: :recycle: add reference initial code

--- src/components/pages/species/show/index.tsx
+++ src/components/pages/species/show/index.tsx
@@ -1,11 +1,36 @@
-import { Box, Grid, GridItem, ListItem, OrderedList, SimpleGrid } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Grid,
+  GridItem,
+  ListItem,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  OrderedList,
+  SimpleGrid,
+  useDisclosure
+} from "@chakra-ui/react";
 import ExternalBlueLink from "@components/@core/blue-link/external";
 import ToggleablePanel from "@components/pages/common/toggleable-panel";
+import { yupResolver } from "@hookform/resolvers/yup";
+import AddIcon from "@icons/add";
+import CheckIcon from "@icons/check";
+import CrossIcon from "@icons/cross";
+import { axCreateSpeciesReferences } from "@services/species.service";
+import useTranslation from "next-translate/useTranslation";
 import React, { useMemo } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import * as Yup from "yup";
 
 import { SpeciesActivity } from "./activity";
 import SpeciesCommonNamesContainer from "./common-names";
 import SpeciesFields from "./fields";
+import ReferencesField from "./fields/field/edit/references-field";
 import { generateReferencesList } from "./fields/field/references/utils";
 import SpeciesGallery from "./gallery";
 import SpeciesGroups from "./group";
@@ -17,16 +42,50 @@ import { SpeciesProvider } from "./use-species";
 
 export default function SpeciesShowPageComponent({ species, permissions, licensesList }) {
   console.debug("Species", species, permissions);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const { t } = useTranslation();
 
   const fieldsRender = useMemo(
     () => generateReferencesList(species.fieldData),
     [species.fieldData]
   );
 
-  // const commonRender = useMemo(
-  //   () => generateReferencesList(species.fieldData),
-  //   [species.fieldData]
-  // );
+  const hFormRef = useForm<any>({
+    resolver: yupResolver(
+      Yup.object().shape({
+        references: Yup.array().of(
+          Yup.object().shape({
+            id: Yup.mixed(),
+            title: Yup.string().required(),
+            url: Yup.string().nullable()
+          })
+        )
+      })
+    ),
+    defaultValues: {
+      references: []
+      // Add default values for references array
+      // references: species.referencesListing || [] // Initialize with existing data
+    }
+  });
+
+  const handleOnSave = async (values) => {
+    const payload: any[] = [];
+
+    values.references.forEach((ref) =>
+      payload.push({
+        title: ref.title,
+        url: ref.url,
+        speciesId: species.species.id
+      })
+    );
+
+    const { success } = await axCreateSpeciesReferences(payload, species.species.id);
+    if (success) {
+      onClose();
+    }
+  };
 
   return (
     <SpeciesProvider species={species} permissions={permissions} licensesList={licensesList}>
@@ -48,6 +107,45 @@ export default function SpeciesShowPageComponent({ species, permissions, license
             */}
 
             <ToggleablePanel id="123" icon="📚" title="References">
+              <Box margin={3}>
+                <Button
+                  variant="outline"
+                  size="xs"
+                  colorScheme="green"
+                  leftIcon={<AddIcon />}
+                  onClick={onOpen}
+                >
+                  {t("common:add")}
+                </Button>
+
+                <Modal isOpen={isOpen} onClose={onClose}>
+                  <ModalOverlay />
+                  <ModalContent>
+                    <FormProvider {...hFormRef}>
+                      <form onSubmit={hFormRef.handleSubmit(handleOnSave)}>
+                        <ModalHeader>References</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                          <ReferencesField name="references" label={t("species:references")} />
+                        </ModalBody>
+                        <ModalFooter>
+                          <Button type="submit" colorScheme="blue" leftIcon={<CheckIcon />}>
+                            {t("common:save")}
+                          </Button>
+                          <Button
+                            ml={4}
+                            leftIcon={<CrossIcon />}
+                            onClick={onClose}
+                            type="button" // Important: specify type="button" to prevent form submission
+                          >
+                            {t("common:cancel")}
+                          </Button>
+                        </ModalFooter>
+                      </form>
+                    </FormProvider>
+                  </ModalContent>
+                </Modal>
+              </Box>
               <Box p={4} pb={0}>
                 {fieldsRender.map(([path, references]) => (
                   <Box key={path} mb={3}>
--- src/services/species.service.ts
+++ src/services/species.service.ts
@@ -252,3 +252,15 @@ export const axGetSpeciesIdFromTaxonId = async (taxonId) => {
     return { success: false, data: {} };
   }
 };
+
+export const axCreateSpeciesReferences = async (payload, speciesId) => {
+  try {
+    const { data } = await http.post(
+      `${ENDPOINT.SPECIES}/v1/species/add/reference/${speciesId}`,
+      payload
+    );
+    return { success: true, data };
+  } catch (e) {
+    return { success: false, data: null };
+  }
+};
