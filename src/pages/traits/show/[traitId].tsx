b40cd3e073b5b42467facea53dfda904aa7402bf
mekala.ravi <mekala.ravi@strandls.com>
2025-03-10 14:54:49 +0530

    Fixed traits show bug

--- src/pages/traits/show/[traitId].tsx
+++ src/pages/traits/show/[traitId].tsx
@@ -1,12 +1,11 @@
 import TraitsShowComponent from "@components/pages/traits/show";
-import useGlobalState from "@hooks/use-global-state";
 import { axGetTraitShowData } from "@services/traits.service";
+import { getLanguageId } from "@utils/i18n";
 import React from "react";
 
 const TraitShowPage = ({ data }) => <TraitsShowComponent data={data} />;
 export const getServerSideProps = async (ctx) => {
-  const { languageId } = useGlobalState();
-  const { success, data } = await axGetTraitShowData(ctx.query.traitId, languageId);
+  const { success, data } = await axGetTraitShowData(ctx.query.traitId, getLanguageId(ctx.locale)?.ID);
   return success
     ? {
         props: {
