aae27efebec2fe237f3f5ec9aad82ccd1c8f4d1a
mekala.ravi <mekala.ravi@strandls.com>
2025-05-21 19:23:18 +0530

    fixing code to include time limit out case

--- src/components/pages/observation/list/views/stats/use-unique-species.tsx
+++ src/components/pages/observation/list/views/stats/use-unique-species.tsx
@@ -25,7 +25,7 @@ export default function useUniqueSpecies({ filter, location = "" }) {
     );
 
     setter((_draft) => {
-      if (success) {
+      if (success && data.aggregateStatsData.groupUniqueSpecies!=null) {
         if (reset) {
           _draft.list = Object.entries(data.aggregateStatsData.groupUniqueSpecies);
           _draft.lifelistoffset = LIFE_LIST_LIMIT;
