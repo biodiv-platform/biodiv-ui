1d4a07a2740ef8081038a29302c9a86cffe664a1
mekala.ravi <mekala.ravi@strandls.com>
2025-06-06 19:18:15 +0530

    Removed graphs from show page

--- src/components/pages/species/show/sidebar/index.tsx
+++ src/components/pages/species/show/sidebar/index.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import useSpecies from "../use-species";
 import SpeciesRelatedObservations from "./related-observations";
 import TaxonTable from "./taxon-table";
-import SpeciesTemportalDistribution from "./temporal";
 import SpeciesTraitsGraph from "./traits-graph";
 import useObservationStatsData from "./use-observation-stats-data";
 
@@ -25,7 +24,6 @@ export default function SpeciesSidebar() {
   return (
     <GridItem colSpan={2}>
       <TaxonTable />
-      <SpeciesTemportalDistribution data={data} />
       <SpeciesTraitsGraph data={data} />
       <SpeciesRelatedObservations />
     </GridItem>
