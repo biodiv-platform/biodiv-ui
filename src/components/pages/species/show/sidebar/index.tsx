c52e2080237a8dfc4c4235066c7fbb3d57ae2128
mekala.ravi <mekala.ravi@strandls.com>
2025-06-06 19:25:45 +0530

    Changed code

--- src/components/pages/species/show/sidebar/index.tsx
+++ src/components/pages/species/show/sidebar/index.tsx
@@ -1,30 +1,13 @@
 import { GridItem } from "@chakra-ui/layout";
 import React from "react";
 
-import useSpecies from "../use-species";
 import SpeciesRelatedObservations from "./related-observations";
 import TaxonTable from "./taxon-table";
-import SpeciesTraitsGraph from "./traits-graph";
-import useObservationStatsData from "./use-observation-stats-data";
 
 export default function SpeciesSidebar() {
-  const { species } = useSpecies();
-  let data = {
-    data: {
-      list: {
-        groupTraits: [],
-        groupObservedOn: {}
-      },
-      isLoading: false
-    }
-  };
-  if (species.species.taxonConceptId != null && species.species.taxonConceptId != undefined) {
-    data = useObservationStatsData(species.species.taxonConceptId);
-  }
   return (
     <GridItem colSpan={2}>
       <TaxonTable />
-      <SpeciesTraitsGraph data={data} />
       <SpeciesRelatedObservations />
     </GridItem>
   );
