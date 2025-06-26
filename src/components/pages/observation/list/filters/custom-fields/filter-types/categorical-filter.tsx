2a89fcc1d014c455772f8721d2c40bffa3628fa9
mekala.ravi <mekala.ravi@strandls.com>
2025-06-26 12:43:44 +0530

    Removed has content and no content from filters

--- src/components/pages/observation/list/filters/custom-fields/filter-types/categorical-filter.tsx
+++ src/components/pages/observation/list/filters/custom-fields/filter-types/categorical-filter.tsx
@@ -1,12 +1,10 @@
 import React, { useMemo } from "react";
 
 import CheckboxFilter from "../../shared/checkbox/checkboxs";
-import { CUSTOM_FILTER_KEYS } from "./filter-keys";
 
 export default function CatergoricalFilter({ field: { id, values, name, fieldtype } }) {
   const groupOptions = useMemo(
     () => [
-      ...CUSTOM_FILTER_KEYS,
       ...values.map((g) => ({ label: g.value, value: g.value, stat: g.value }))
     ],
     []
