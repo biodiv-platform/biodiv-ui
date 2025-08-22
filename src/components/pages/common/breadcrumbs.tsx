f38da35f0b0792a0b793def60e589149cf9c67d6
arunasd463 <30496644+arunasd463@users.noreply.github.com>
2025-08-22 11:35:52 +0530

    Chakra migration fix (#400)
    * fix: :bug: nearby and related observations link fix
    
    * refactor: :recycle: hydration fix for observation show
    
    * fix: :bug: null issue fix in observation show page
    
    * fix: :bug: taxon list page filter fix

--- src/components/pages/common/breadcrumbs.tsx
+++ src/components/pages/common/breadcrumbs.tsx
@@ -1,22 +1,26 @@
-import { Box, BreadcrumbItem } from "@chakra-ui/react";
+import { Box, Breadcrumb } from "@chakra-ui/react";
 import LocalLink from "@components/@core/local-link";
 import React from "react";
-import { LuChevronRight } from "react-icons/lu";
-
-import { BreadcrumbLink, BreadcrumbRoot } from "@/components/ui/breadcrumb";
 
 const TaxonBreadcrumbs = ({ crumbs, type }) =>
   crumbs ? (
     <Box p={2} px={4} mb={4} className="white-box fadeInUp delay-2" overflowX="auto">
-      <BreadcrumbRoot gap="8px" separator={<LuChevronRight />}>
-        {crumbs.map(({ name, id }) => (
-          <BreadcrumbItem key={id}>
-            <LocalLink href={`/${type}/list`} params={{ taxon: id }}>
-              <BreadcrumbLink whiteSpace="nowrap">{name}</BreadcrumbLink>
-            </LocalLink>
-          </BreadcrumbItem>
-        ))}
-      </BreadcrumbRoot>
+      <Breadcrumb.Root gap="8px">
+        <Breadcrumb.List>
+          {crumbs.map(({ name, id }) => (
+            <>
+              <Breadcrumb.Item key={id}>
+                <Breadcrumb.Link whiteSpace="nowrap" asChild>
+                  <LocalLink href={`/${type}/list`} params={{ taxon: id }}>
+                    {name}
+                  </LocalLink>
+                </Breadcrumb.Link>
+              </Breadcrumb.Item>
+              <Breadcrumb.Separator />
+            </>
+          ))}
+        </Breadcrumb.List>
+      </Breadcrumb.Root>
     </Box>
   ) : null;
 
