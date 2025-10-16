8a265681f48e9c23d2904666811255b8640356a4
arunasd463 <30496644+arunasd463@users.noreply.github.com>
2025-10-16 17:04:56 +0530

    feat: :sparkles: maplibre migration feature (#416)

--- src/components/pages/document/list/filters/location/map-area/map-draw-container.tsx
+++ src/components/pages/document/list/filters/location/map-area/map-draw-container.tsx
@@ -1,14 +1,15 @@
 import { Box } from "@chakra-ui/react";
 import useDocumentFilter from "@components/pages/document/common/use-document-filter";
-import SITE_CONFIG from "@configs/site-config";
-import { getMapCenter, stringToFeature } from "@utils/location";
+import { convertFeatureToPolygonString, getMapCenter, stringToFeature } from "@utils/location";
 import dynamic from "next/dynamic";
 import React, { useMemo } from "react";
 
+import { mapStyles } from "@/static/constants";
+
 const FILTER_NAME = "location";
 
-const NakshaMapboxDraw: any = dynamic(
-  () => import("naksha-components-react").then((mod: any) => mod.NakshaMapboxDraw),
+const NakshaMaplibreDraw: any = dynamic(
+  () => import("naksha-components-react").then((mod: any) => mod.NakshaMaplibreDraw),
   {
     ssr: false,
     loading: () => <p>Loading...</p>
@@ -21,22 +22,30 @@ export default function MapDrawContainer() {
   const defaultViewState = useMemo(() => getMapCenter(2.8), []);
 
   const handleOnFeatureChange = (features) => {
-    if (features.length > 0) {
-      addFilter(FILTER_NAME, features[0]?.geometry?.coordinates.toString());
-      addFilter("geoShapeFilterField", "documentCoverages.topology");
+    if (!features.length) {
+      removeFilter(FILTER_NAME);
+      removeFilter("geoShapeFilterField");
       return;
     }
-    removeFilter(FILTER_NAME);
-    removeFilter("geoShapeFilterField");
+
+    const polygonString = convertFeatureToPolygonString(features[0]);
+
+    if (polygonString) {
+      addFilter(FILTER_NAME, polygonString);
+      addFilter("geoShapeFilterField", "documentCoverages.topology");
+    } else {
+      removeFilter(FILTER_NAME);
+      removeFilter("geoShapeFilterField");
+    }
   };
 
   return (
     <Box position="relative" h="22rem">
-      <NakshaMapboxDraw
+      <NakshaMaplibreDraw
         defaultViewState={defaultViewState}
         features={defaultFeatures}
-        mapboxAccessToken={SITE_CONFIG.TOKENS.MAPBOX}
         onFeaturesChange={handleOnFeatureChange}
+        mapStyles={mapStyles}
         isPolygon={true}
       />
     </Box>
