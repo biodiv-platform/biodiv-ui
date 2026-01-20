6faf2e510a43f65357fc81a11d637fbd1365e40b
Arunbala Balasubramani <arunbala.balasubramani@strandls.com>
2026-01-20 11:58:26 +0530

    migrated pages gallery slider

--- src/components/pages/page/show/header/page-slider.tsx
+++ src/components/pages/page/show/header/page-slider.tsx
@@ -1,8 +1,5 @@
-import "@splidejs/react-splide/css";
-
-import { Box } from "@chakra-ui/react";
+import { Box, Carousel } from "@chakra-ui/react";
 import { axGetLicenseList } from "@services/resources.service";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
 import { RESOURCE_SIZE } from "@static/constants";
 import { getResourceThumbnail, RESOURCE_CTX } from "@utils/media";
 import React, { useEffect, useState } from "react";
@@ -10,10 +7,9 @@ import React, { useEffect, useState } from "react";
 interface PageSliderProps {
   images?;
   description;
-  pageId;
 }
 
-export function PageSlider({ images, description, pageId }: PageSliderProps) {
+export function PageSlider({ images, description }: PageSliderProps) {
   const [licenses, setLicenses] = useState({});
 
   useEffect(() => {
@@ -24,41 +20,41 @@ export function PageSlider({ images, description, pageId }: PageSliderProps) {
 
   return (
     <>
-      <Box position="absolute" top={0} left={0} bottom={0} right={0}>
-        <Splide
-          options={{
-            fixedHeight: 300,
-            type: "fade",
-            pagination: false,
-            arrows: false,
-            autoplay: true,
-            rewind: true
-          }}
-          key={pageId}
-        >
-          {images.map((image) => (
-            <SplideSlide key={image.id}>
-              <img
-                src={getResourceThumbnail(RESOURCE_CTX.PAGES, image.fileName, RESOURCE_SIZE.PAGE)}
-                alt={image.id}
-              />
-              {description}
-              <Box
-                position="absolute"
-                top={0}
-                left={0}
-                bottom={0}
-                right={0}
-                bgGradient="linear(to-r, blackAlpha.500, blackAlpha.400)"
-              />
-              {image.attribution && (
-                <Box position="absolute" left={0} bottom={0} p={4}>
-                  {image.caption} by {image.attribution} ({licenses[image.licenseId]})
-                </Box>
-              )}
-            </SplideSlide>
-          ))}
-        </Splide>
+      <Box position="absolute" inset={0} w="full" h="full" overflow="hidden">
+        <Carousel.Root autoplay loop slideCount={images.length} w="full" h="full">
+          <Carousel.Control w="full" h="full" position="relative">
+            <Carousel.ItemGroup w="full" h="full">
+              {images.map((image, index) => (
+                <Carousel.Item key={image.id} index={index} w="full" h="full" position="relative">
+                  <Box
+                    position="absolute"
+                    inset={0}
+                    bgImage={`url(${getResourceThumbnail(
+                      RESOURCE_CTX.PAGES,
+                      image.fileName,
+                      RESOURCE_SIZE.PAGE
+                    )})`}
+                    bgSize="cover"
+                  />
+
+                  <Box
+                    position="absolute"
+                    inset={0}
+                    bgGradient="linear(to-r, blackAlpha.500, blackAlpha.400)"
+                  />
+
+                  {description}
+
+                  {image.attribution && (
+                    <Box position="absolute" left={0} bottom={0} p={4}>
+                      {image.caption} by {image.attribution} ({licenses[image.licenseId]})
+                    </Box>
+                  )}
+                </Carousel.Item>
+              ))}
+            </Carousel.ItemGroup>
+          </Carousel.Control>
+        </Carousel.Root>
       </Box>
     </>
   );
