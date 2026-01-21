dcf33fca0c233e17e02bba9ad65f5a12899882c0
Arunbala Balasubramani <arunbala.balasubramani@strandls.com>
2026-01-21 11:48:07 +0530

    fix: :bug: species name linebreak bug fix

--- src/components/@core/scientific-name/index.tsx
+++ src/components/@core/scientific-name/index.tsx
@@ -2,5 +2,12 @@ import { getInjectableScientificName } from "@utils/text";
 import React from "react";
 
 export default function ScientificName({ value }) {
-  return <span dangerouslySetInnerHTML={getInjectableScientificName(value)} />;
+  return (
+    <span
+      style={{
+        wordBreak: "break-word"
+      }}
+      dangerouslySetInnerHTML={getInjectableScientificName(value)}
+    />
+  );
 }
