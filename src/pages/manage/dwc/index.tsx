895a8275dcf25e7b066f2162a268d76461fd2971
arunasd463 <30496644+arunasd463@users.noreply.github.com>
2026-02-05 16:28:23 +0530

    fix: :bug: fixed dwc list bug (#448)

--- src/pages/manage/dwc/index.tsx
+++ src/pages/manage/dwc/index.tsx
@@ -1,33 +1,22 @@
 import React from "react";
 
-import { authorizedPageSSR } from "@/components/auth/auth-redirect";
+import { authorizedPageSSP } from "@/components/auth/auth-redirect";
 import GbifExportTable from "@/components/pages/dwc";
 import { DwcLogsDataProvider } from "@/components/pages/dwc/use-dwc-filter-log";
 import { Role } from "@/interfaces/custom";
-import { axListDwc } from "@/services/files.service";
 
-const Dwc = ({ DwcLogData, initialFilterParams }) => (
-  <DwcLogsDataProvider filter={initialFilterParams} DwcLogData={DwcLogData}>
+const Dwc = ({ initialFilterParams }) => (
+  <DwcLogsDataProvider filter={initialFilterParams}>
     <GbifExportTable />
   </DwcLogsDataProvider>
 );
 
 export const getServerSideProps = async (ctx) => {
-  authorizedPageSSR([Role.Admin], ctx, false);
-
-  const offset = parseInt(ctx.query.offset || "0", 10);
-
-  const { data } = await axListDwc({ offset, limit: 15 }, ctx);
+  const authResult = authorizedPageSSP([Role.Admin], ctx);
+  if (authResult) return authResult;
 
   return {
     props: {
-      DwcLogData: {
-        l: data.files || [],
-        n: data.total,
-        hasMore: data.total > offset + 15,
-        filePath: data.filePath
-      },
-      nextOffset: offset + 15,
       initialFilterParams: { ...ctx.query }
     }
   };
