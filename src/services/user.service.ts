67813dbf6ae3c76c3383f6b62c512dce7ad333ad
prakhar-s <prakhardevsingh4@gmail.com>
2026-05-04 09:42:31 +0530

    changing http method from post to get

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -163,8 +163,8 @@ export const axGetUserList = async (
 ) => {
   const httpReq = isAdmin ? http : plainHttp;
   try {
-    const { data } = await httpReq.post(`${ENDPOINT.USER}/v1/user/list/${index}/${type}`, payload, {
-      params
+    const { data } = await httpReq.get(`${ENDPOINT.USER}/v1/user/list/${index}/${type}`, {
+      params: { ...params, ...payload }
     });
 
     return { success: true, data };
