65295c5a0c252fa1282b3e7cee6d9d0230b3bfad
rishitha-ravi <mekala.ravi@strandls.com>
2026-05-07 17:01:33 +0530

    :bug: fixed trait filter in observation and removed unneccesary calls (#458)

--- src/components/pages/species/list/filter/name-of-user/index.tsx
+++ src/components/pages/species/list/filter/name-of-user/index.tsx
@@ -17,6 +17,7 @@ export default function UserFilterInput({ filterKey }) {
   const onQuery = debounce(axUserFilterSearch, 200);
 
   useEffect(() => {
+    if (!filter?.[filterKey]) return;
     axGetUsersByID(filter?.[filterKey]).then(setDefaultValue);
   }, []);
 
