dbe8b3b4ee3234dd71a4f7ad083e33fe48e05e64
Arunbala Balasubramani <arunbala.balasubramani@strandls.com>
2026-07-09 18:07:49 +0530

    test: :alembic: testing nextjs image optimiser

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const nextTranslate = require("next-translate-plugin");
 const { SITE } = require("./src/configs/site-config");
+
 const siteUrl = new URL(SITE.SSR_URL);
 const mainDomain = siteUrl.hostname;
 
@@ -9,18 +10,10 @@ module.exports = nextTranslate({
     nextScriptWorkers: false
   },
   images: {
-    dangerouslyAllowLocalIP: !!process.env.NEXT_PUBLIC_DEV_LOCAL_HOSTS,
+    dangerouslyAllowLocalIP: true,
     remotePatterns: [
-      {
-        protocol: siteUrl.protocol.replace(":", ""),
-        hostname: mainDomain,
-        pathname: "/**"
-      },
-      {
-        protocol: "https",
-        hostname: `*.${mainDomain}`,
-        pathname: "/**"
-      }
+      { protocol: siteUrl.protocol.replace(":", ""), hostname: mainDomain, pathname: "/**" },
+      { protocol: "https", hostname: `*.${mainDomain}`, pathname: "/**" }
     ]
   }
 }, { turbopack: true });
